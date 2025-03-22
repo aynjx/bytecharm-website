db29e1e8fcc44890de6130aa9bcfe3277e62ce2a
aynjx <ajsarreal23@gmail.com>
2025-03-23 00:16:30 +0800

    Add files via upload

---
+++ images.bat
@@ -0,0 +1,12 @@
+/your-project-folder
+  ├── c:\Users\user\ByteCharm Site\index.html 
+  ├── c:\Users\user\ByteCharm Site\style.css
+  ├── images/
+      ├── c:\Users\user\ByteCharm Site\bracelet1.png 
+      ├── c:\Users\user\ByteCharm Site\bracelet2.png
+      ├── c:\Users\user\ByteCharm Site\bracelet3.png
+      ├── c:\Users\user\ByteCharm Site\bracelet4.png
+      ├── c:\Users\user\ByteCharm Site\necklace5.png
+      ├── c:\Users\user\ByteCharm Site\necklace6.png
+      ├── c:\Users\user\ByteCharm Site\necklace7.png
+      ├── c:\Users\user\ByteCharm Site\necklace8.png
